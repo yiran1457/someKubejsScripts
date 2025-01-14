0276823726a6779da074752cc1e6a43a905cd471
yi_ran <164875652+yiran1457@users.noreply.github.com>
2025-01-15 01:15:33 +0800

    Update Re.AutoTooltip.js

--- client_scripts/src/yi/Re.AutoTooltip.js
+++ client_scripts/src/yi/Re.AutoTooltip.js
@@ -1,18 +1,36 @@
+let  $GLFW  = Java.loadClass("org.lwjgl.glfw.GLFW")
 
-
+/**
+ * 
+ * @param {'Q'|'W'|'E'|'R'|'T'|'Y'|'U'|'I'|'O'|'P'|
+ * 'A'|'S'|'D'|'F'|'G'|'H'|'J'|'K'|'L'|
+ * 'Z'|'X'|'C'|'V'|'B'|'N'|'M'|
+ * '0'|'1'|'2'|'3'|'4'|'5'|'6'|'7'|'8'|'9'|'SPACE'
+ * } keyname 
+ * @returns 
+ */
+let keydown = (keyname)=>Client.isKeyDown($GLFW[`GLFW_KEY_${keyname}`]
+                                          
+let ToCheckKey = (/**@type {string} */key) => {
+    if(key.indexOf('no')==-1)
+        return keydown(key)?'T':'F'
+    return !keydown(key.substring(2))?'T':'F'
+}
+                                          
 //根据lang来自动生成tooltip
 let tooltip_path
 //设置lang的命名空间
 let namespace = 'autotooltip'
 //设置默认lang类型
 let deflang = 'en_us'
-//设置默认lang路径
-let deflang_path = 'kubejs/assets/' + namespace + '/lang/' + deflang + '.json'
 
-ClientEvents.loggedIn(e => {
-    //获取客户端对应lang的路径
-    tooltip_path = 'kubejs/assets/' + namespace + '/lang/' + Client.languageManager.selected + '.json'
-})
+
+
+//根据lang来自动生成tooltip
+//设置lang的命名空间
+let namespace = 'autotooltip'
+//设置默认lang类型
+let deflang = 'en_us'
 
 /*
     lang键以modid.itemid为开头，tooltip为结尾，中间为检测按键，不可错填
@@ -25,15 +43,13 @@ ClientEvents.loggedIn(e => {
         "minecraft.iron_ingot.LEFT_CONTROL.M.tooltip":"ctrl+n"
     }
  */
-let ToCheckKey = (/**@type {string} */key) => {
-    if(key.indexOf('no')==-1)
-        return keydown(key)?'T':'F'
-    return !keydown(key.substring(2))?'T':'F'
-}
 
 ItemEvents.tooltip(e => {
     //合并默认lang和客户端lang，使客户端lang缺失时使用默认lang
-    Object.assign(JsonIO.read(deflang_path), JsonIO.read(tooltip_path))
+    Object.assign(
+        JsonIO.read('kubejs/assets/' + namespace + '/lang/' + deflang + '.json'), 
+        JsonIO.read('kubejs/assets/' + namespace + '/lang/' + Client.languageManager.selected + '.json')
+    )
         .forEach(/**@param {String} langkey*/langkey => {
             //切分键名成数组
             let keysplit = langkey.split('.')
