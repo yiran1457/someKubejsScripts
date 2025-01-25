ddd3b9e640de24a9baff629b2cc932290f84d524
yiran1457 <3270897489@qq.com>
2025-01-25 14:36:45 +0800

    common

---
+++ server_scripts/src/test/share.js
@@ -0,0 +1,60 @@
+// ignored : true
+ForgeEvents.onEvent($ItemAttributeModifierEvent,/**@param {$ItemAttributeModifierEvent_} event */event => {
+
+                let itemStack = event.getItemStack();
+                let slotType = event.getSlotType();
+                let attributeTag = itemStack.hasTag('mryh:random_attribute')
+                let nbt = itemStack.nbt;
+                if (itemStack.id == 'minecraft:diamond_hoe') {
+                    console.log('物品带有自定义标签');
+                    if (nbt.contains('mryh_attribute')) {
+                        console.log('物品有 NBT 数据');
+                        let customAttribute = nbt.get('mryh_attribute'); 
+                        console.log(`获取到的 NBT 属性 ${customAttribute}`);
+                        switch (customAttribute) {
+                            case 'strength':
+                                event.addModifier(
+                                    'minecraft:generic.attack_damage',
+                                    new $AttributeModifier(
+                                        $UUID.randomUUID(),
+                                        'strength_modifier',
+                                        5, 
+                                        'addition'
+                                    )
+                                );
+                                console.log('物品具有 "strength" 属性');
+                                break;
+                            case 'speed':
+                                event.addModifier(
+                                    'minecraft:generic.attack_speed',
+                                    new $AttributeModifier(
+                                        $UUID.randomUUID(),
+                                        'speed_modifier',
+                                        2, 
+                                        'addition'
+                                    )
+                                );
+                                console.log('物品具有 "speed" 属性');
+                                break;
+            
+                            case 'durability':
+                                event.addModifier(
+                                    'minecraft:generic.max_health',
+                                    new $AttributeModifier(
+                                        $UUID.randomUUID(),
+                                        'durability_modifier',
+                                        10, 
+                                        'addition'
+                                    )
+                                );
+                                console.log('物品具有 "durability" 属性');
+                                break;
+                            default:
+                                console.log('未识别的 NBT 属性');
+                                break;
+                        }
+                    } else {
+                        console.log('物品没有 NBT 数据');
+                    }
+                }
+})
--- startup_scripts/src/yi/一些奇怪的注册.js
+++ startup_scripts/src/yi/一些奇怪的注册.js
@@ -1,5 +1,9 @@
 //priority:100
 
+const { $UUID } = require("packages/java/util/$UUID")
+const { $AttributeModifier } = require("packages/net/minecraft/world/entity/ai/attributes/$AttributeModifier")
+const { $ItemAttributeModifierEvent } = require("packages/net/minecraftforge/event/$ItemAttributeModifierEvent")
+
 
 
 
@@ -11,3 +15,26 @@ $Rarity.create('yi', "aqua")
 $Rarity.create('evil', "dark_red")
 $Rarity.create('holy', "gold")
 
+
+ForgeEvents.onEvent($ItemAttributeModifierEvent,/**@param {$ItemAttributeModifierEvent_} event */event => {/*
+    let itemStack = event.getItemStack()
+    if (!itemStack.isEnchanted()) return
+    if (itemStack.getItem() instanceof $ArmorItem ) {
+        if (!(event.getSlotType() == itemStack.getItem().getEquipmentSlot())) {
+            return;
+        }
+    } else if (event.getSlotType() != $EquipmentSlot.MAINHAND && event.getSlotType() != $EquipmentSlot.OFFHAND) {
+        return;
+    } else if (itemStack.getItem() instanceof $ShieldItem && !(event.getSlotType() == $EquipmentSlot.OFFHAND))
+        return;
+
+    if (itemStack.getEnchantmentLevel('minecraft:protection') > 0)
+        event.addModifier("generic.attack_speed",
+            new $AttributeModifier(
+                $UUID.randomUUID(),
+                'test',
+                0.4 * itemStack.getEnchantmentLevel('minecraft:protection'),
+                'addition'
+            ))*/
+
+})
