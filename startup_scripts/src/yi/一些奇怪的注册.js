989a55235120ffa07f48c86d255003d0672d3d52
yiran1457 <3270897489@qq.com>
2025-01-25 15:18:48 +0800

    common

--- startup_scripts/src/yi/一些奇怪的注册.js
+++ startup_scripts/src/yi/一些奇怪的注册.js
@@ -4,8 +4,6 @@ const { $UUID } = require("packages/java/util/$UUID")
 const { $EquipmentSlot } = require("packages/net/minecraft/world/entity/$EquipmentSlot")
 const { $AttributeModifier } = require("packages/net/minecraft/world/entity/ai/attributes/$AttributeModifier")
 const { $ArmorItem } = require("packages/net/minecraft/world/item/$ArmorItem")
-const { $ShieldItem } = require("packages/net/minecraft/world/item/$ShieldItem")
-const { $ItemAttributeModifierEvent } = require("packages/net/minecraftforge/event/$ItemAttributeModifierEvent")
 
 
 
