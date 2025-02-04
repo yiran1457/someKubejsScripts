9c84451777714dfaceff986067db6f6ae8282d75
yiran1457 <3270897489@qq.com>
2025-02-04 16:58:50 +0800

    黑魔法！

--- server_scripts/src/Mytool/loadClassStream/BrokenClassFilter.js
+++ server_scripts/src/Mytool/loadClassStream/BrokenClassFilter.js
@@ -1,6 +1,6 @@
 //priority:888888
 
-const { $ClassFilter } = require("packages/dev/latvian/mods/kubejs/util/$ClassFilter")
+const $ClassFilter = Java.loadClass("dev.latvian.mods.kubejs.util.ClassFilter")
 
 /**@type {$Class_<T>} */
 let $Class = $ClassFilter.__javaObject__
@@ -9,4 +9,4 @@ sm.setAccessible(true)
 sm = sm.get(Java)
 let mana = $Class.classLoader.loadClass('dev.latvian.mods.kubejs.script.ScriptManager').getDeclaredField('classFilter')
 mana.setAccessible(true)
-mana.set(sm,$ClassFilter())
+mana.set(sm, $ClassFilter())
