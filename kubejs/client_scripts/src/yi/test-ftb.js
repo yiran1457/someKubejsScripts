1a5f86d5e46d31fd650c94fffeecadda5a723a0a
yiran1457 <3270897489@qq.com>
2025-03-27 17:07:17 +0800

    chore: 更新 .gitignore 并添加新配置
    - 在 .gitignore 中添加 KubeJS 语言文件
    - 更新 JEI 配置，添加新配方分类
    - 在侧边栏按钮配置中添加 FTBEssentials 和 FTCChunks 相关按钮

---
+++ kubejs/client_scripts/src/yi/test-ftb.js
@@ -0,0 +1,41 @@
+
+//用于找出所有的id
+console.log(Client.resourceManager.getNamespaces())
+
+//写出未翻译语言文件在指定文件夹
+writeLang("art_of_forging", 'kubejs/lang/')
+/**
+ * @param {string} modid 
+ * @param {string} path
+ */
+function writeLang(modid, path) {
+  let en_us = readLang(modid, 'en_us')
+  if (en_us == null) return
+  let zh_cn = readLang(modid, 'zh_cn')
+  let returnLang = {}
+  if (zh_cn != null) {
+    en_us.forEach((k, v) => {
+      if (zh_cn.get(k)) return
+      returnLang[k] = v
+    })
+  } else (
+    returnLang = en_us
+  )
+  JsonIO.write(path + modid + '.json', returnLang)
+}
+/**
+ * @param {string} modid 
+ * @param {string} lang 
+ * @returns {$HashMap_<string,string>||null}
+ */
+function readLang(modid, lang) {
+  let langFile = Client.resourceManager.getResource(modid + ':lang/' + lang + '.json').orElse(null)
+  if (langFile != null) {
+    let t = ''
+    langFile.openAsReader().lines().toList().forEach(v => {
+      t += v
+    })
+    langFile = JsonIO.parse(t)
+  }
+  return langFile
+}
--- kubejs/startup_scripts/src/yi/startuptest.js
+++ kubejs/startup_scripts/src/yi/startuptest.js
@@ -4,3 +4,47 @@ StartupEvents.registry('enchantment',e=>{
 KeyBindEvents.register(event=>{
     event.create('ftb-item','key.ftb-item',GLFW.GLFW_KEY_I,'key.category.common')
 })
+
+
+if (true)//删除按键
+{
+    let info = ''
+    KeyBindUtil.getAllKeyName().forEach(text => {
+        let name = text.slice(0, text.indexOf(":") - 1)
+        let key = text.slice(text.indexOf(":") + 2)
+        info += `
+event.remove('${key}') //${name}`
+    })
+    console.log(info)
+}
+if (false)//修改默认按键
+{
+    let info = ''
+    KeyBindUtil.getAllKeyName().forEach(text => {
+        let name = text.slice(0, text.indexOf(":") - 1)
+        let key = text.slice(text.indexOf(":") + 2)
+        info += `
+event.modifyKey('${key}',-1) //${name}`
+    })
+    console.log(info)
+}
+
+
+KeyBindEvents.modify(event=>{
+    event.remove('key.socialInteractions') //社交屏幕
+    event.remove('key.screenshot') //截图
+    event.remove('key.smoothCamera') //切换电影视角
+    event.remove('key.fullscreen') //切换全屏显示
+    event.remove('key.spectatorOutlines') //高亮玩家（旁观者）
+    event.remove('key.saveToolbarActivator') //保存快捷栏
+    event.remove('key.loadToolbarActivator') //加载快捷栏
+    event.remove('tetra.toolbelt.binding.access') //Quick access
+    event.remove('tetra.toolbelt.binding.restock') //Restock toolbelt
+    event.remove('tetra.toolbelt.binding.open') //打开工具带
+    event.remove('tetra.toolbelt.binding.secondary_use') //Use secondary
+    event.remove('key.jei.toggleCheatModeConfigButton') //切换作弊模式
+    event.remove('keybinds.bettercombat.feint') //卖个破绽
+    event.remove('key.modernui.openCenter') //打开操作中心
+
+    event.addListener("jump","key.jump")
+})
